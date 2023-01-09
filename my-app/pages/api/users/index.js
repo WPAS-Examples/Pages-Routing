2e506cb653069093ae1f86adf4cb6a2f56fcca30
Patrick Crawford <patrick.crawford@senecacollege.ca>
2023-01-08 23:32:33 -0500

    initial commit

---
+++ my-app/pages/api/users/index.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+
+  const { name } = req.body;
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      // Read data from your database
+      res.status(200).json({ message: `TODO: Get All Users` })
+      break
+    case 'POST':
+      // Create data in your database
+      res.status(200).json({ message: `TODO: Create User with Name: ${name}` })
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
