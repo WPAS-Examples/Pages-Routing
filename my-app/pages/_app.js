302a9f433a408e25af5afc19f50c369cde83a9be
Patrick Crawford <patrick.crawford@senecapolytechnic.ca>
2024-05-09 22:54:14 -0400

    updated to Next 14

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,13 +1,6 @@
-import Layout from '../components/layout'
-import '../styles/globals.css'
+import Layout from "@/components/layout";
+import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+export default function App({ Component, pageProps }) {
+  return <Layout><Component {...pageProps} /></Layout>;
 }
-
-export default MyApp
